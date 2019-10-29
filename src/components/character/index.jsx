470548a54e9fd1182ebf16d86b3a65758c7f8d09
Mark Tellez <mark.tellez@icloud.com>
2019-10-28 22:52:54 -0600

    walk animation

--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import useEventListener from "@use-it/event-listener"
 
+const MAX_STEP = 3
 const SIZE = 32
 const DIRECTION = {
   DOWN: 0,
@@ -10,22 +11,37 @@ const DIRECTION = {
 }
 export default function Character() {
   const offset = { left: 0, top: 0 }
-  const [facing, setFacing] = useState(DIRECTION.DOWN)
+  const [facing, setFacing] = useState({
+    current: DIRECTION.DOWN,
+    previous: DIRECTION.DOWN
+  })
+  const [step, setStep] = useState(0)
 
   useEventListener("keydown", ({ code }) => {
     if (code.indexOf("Arrow") === -1) return
     const direction = DIRECTION[code.replace("Arrow", "").toUpperCase()]
 
-    setFacing(direction)
+    setFacing(prev => ({
+      current: direction,
+      previous: prev.current
+    }))
   })
+
+  useEffect(() => {
+    if (facing.current === facing.previous) {
+      setStep(prev => (prev < MAX_STEP - 1 ? prev + 1 : 0))
+    } else {
+      setStep(0)
+    }
+  }, [facing])
 
   return (
     <div
       style={{
         width: SIZE,
         height: SIZE,
-        background: `url(/characters.png) -${offset.left}px -${offset.top +
-          facing}px`
+        background: `url(/characters.png) -${offset.left +
+          step * SIZE}px -${offset.top + facing.current}px`
       }}
     />
   )
