86ed6ab38d76f7cf364e45b82c8780ccef58c3c3
Mark Tellez <mark.tellez@icloud.com>
2019-10-28 22:43:24 -0600

    added event listener hook

--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -1,7 +1,14 @@
 import React from "react"
+import useEventListener from "@use-it/event-listener"
 
 export default function Character() {
   const offset = { left: 0, top: 0 }
+
+  useEventListener("keydown", ({ code }) => {
+    if (code.indexOf("Arrow") === -1) return
+    const direction = code.replace("Arrow", "")
+    console.dir(direction)
+  })
 
   return (
     <div
