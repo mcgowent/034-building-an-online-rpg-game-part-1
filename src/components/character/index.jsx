1a00c79b8508644dba81523a6c4998f32079d4dc
Mark Tellez <mark.tellez@icloud.com>
2019-10-28 22:47:42 -0600

    changing direction

--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -1,21 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import useEventListener from "@use-it/event-listener"
 
+const SIZE = 32
+const DIRECTION = {
+  DOWN: 0,
+  LEFT: SIZE * 1,
+  RIGHT: SIZE * 2,
+  UP: SIZE * 3
+}
 export default function Character() {
   const offset = { left: 0, top: 0 }
+  const [facing, setFacing] = useState(DIRECTION.DOWN)
 
   useEventListener("keydown", ({ code }) => {
     if (code.indexOf("Arrow") === -1) return
-    const direction = code.replace("Arrow", "")
-    console.dir(direction)
+    const direction = DIRECTION[code.replace("Arrow", "").toUpperCase()]
+
+    setFacing(direction)
   })
 
   return (
     <div
       style={{
-        width: 32,
-        height: 32,
-        background: `url(/characters.png) -${offset.left}px -${offset.top}px`
+        width: SIZE,
+        height: SIZE,
+        background: `url(/characters.png) -${offset.left}px -${offset.top +
+          facing}px`
       }}
     />
   )
