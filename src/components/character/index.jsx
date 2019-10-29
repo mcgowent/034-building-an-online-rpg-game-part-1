9efa6bde4873b648c058798c8f3e36059983f574
Mark Tellez <mark.tellez@icloud.com>
2019-10-28 22:56:36 -0600

    done

--- src/components/character/index.jsx
+++ src/components/character/index.jsx
@@ -1,39 +1,12 @@
-import React, { useState, useEffect } from "react"
-import useEventListener from "@use-it/event-listener"
+import React from "react"
 
-const MAX_STEP = 3
-const SIZE = 32
-const DIRECTION = {
-  DOWN: 0,
-  LEFT: SIZE * 1,
-  RIGHT: SIZE * 2,
-  UP: SIZE * 3
-}
+import useWalker from "./use-walker"
+
+import { SIZE } from "../../constants"
+
 export default function Character() {
   const offset = { left: 0, top: 0 }
-  const [facing, setFacing] = useState({
-    current: DIRECTION.DOWN,
-    previous: DIRECTION.DOWN
-  })
-  const [step, setStep] = useState(0)
-
-  useEventListener("keydown", ({ code }) => {
-    if (code.indexOf("Arrow") === -1) return
-    const direction = DIRECTION[code.replace("Arrow", "").toUpperCase()]
-
-    setFacing(prev => ({
-      current: direction,
-      previous: prev.current
-    }))
-  })
-
-  useEffect(() => {
-    if (facing.current === facing.previous) {
-      setStep(prev => (prev < MAX_STEP - 1 ? prev + 1 : 0))
-    } else {
-      setStep(0)
-    }
-  }, [facing])
+  const { facing, step } = useWalker()
 
   return (
     <div
